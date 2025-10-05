46c6695e83984cf3e584bd11c841b35fe103139f
alperdigital <alperdigital@icloud.com>
2025-10-05 23:04:31 +0300

    Sosyal medya baglantilari guncellendi: GitHub, X (Twitter), ve email

--- js/lib/sharing.js
+++ js/lib/sharing.js
@@ -13,9 +13,9 @@ window.addEventListener("load",function(){
 	// Create full html
 	var sharing = document.createElement("div");
 	sharing.className = "sharing";
-	sharing.innerHTML = '<a href="https://www.facebook.com/sharer/sharer.php?u='+link+'&t='+text+'" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="social/facebook.png"></a>'+
-						'<a href="https://twitter.com/intent/tweet?source='+link+'&text='+text+'%20'+link+'" target="_blank" title="Tweet"><img alt="Tweet" src="social/twitter.png"></a>'+
-						'<a href="mailto:?subject='+title+'&body='+text+" "+link+'" target="_blank" title="Send email"><img alt="Send email" src="social/email.png"></a>';
+	sharing.innerHTML = '<a href="https://github.com/alperdigital" title="GitHub" target="_blank"><img alt="GitHub" src="social/facebook.png"></a>'+
+						'<a href="https://x.com/alperdigital" target="_blank" title="X (Twitter)"><img alt="X (Twitter)" src="social/twitter.png"></a>'+
+						'<a href="mailto:alperdigital@icloud.com?subject='+title+'&body='+text+" "+link+'" target="_blank" title="Send email"><img alt="Send email" src="social/email.png"></a>';
 
 	// Replace it in the dom
 	sharingDOM.parentNode.replaceChild(sharing, sharingDOM);
