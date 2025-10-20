8ce7734e5b245341a5ac599818bcaa808354d5bc
alperdigital <alperdigital@icloud.com>
2025-10-20 17:12:01 +0300

    fix(11): make iframe fullscreen and visible - increase size, fix positioning, show main content

--- js/slides/10_Slides_Environment.js
+++ js/slides/10_Slides_Environment.js
@@ -5,10 +5,10 @@ SLIDES.push({
 		// Mount isolated iframe for 11th section (fully sandboxed globals)
 		var sandbox = createIsolatedRuntime({
 			src: "env11.html",
-			width: 500,
-			height: 500,
-			left: -20,
-			top: -20
+			width: 1000,
+			height: 600,
+			left: 0,
+			top: 0
 		});
 		self.dom.appendChild(sandbox.iframe);
 		self.objects.environment_sandbox = sandbox;
