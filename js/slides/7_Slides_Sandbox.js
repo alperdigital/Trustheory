bde49be6a844565cfcd0614c2f3852a735557c5e
alperdigital <alperdigital@icloud.com>
2025-10-26 02:17:24 +0300

    Fix Section 7: Restore original Sandbox slide without group mode

--- js/slides/7_Slides_Sandbox.js
+++ js/slides/7_Slides_Sandbox.js
@@ -2,50 +2,29 @@ SLIDES.push({
 
 	id: "sandbox",
 	onstart: function(self){
-		
+
 		// The tournament simulation
 		Tournament.resetGlobalVariables();
-		
-		// Enable group mode for Bölüm 7 - grup bazlı seçilim
-		Tournament.GROUP_MODE = true;
-		Tournament.GROUP_SIZE = 3;
-		Tournament.HOMOGENEOUS_GROUPS = true;
-		Tournament.GROUP_VOTE_SOURCE = "perMemberHistory";
-		Tournament.GROUP_FITNESS_METRIC = "avg_payoff";
-		
-		// Adjust population to be odd multiple of 3
-		var totalAgents = 0;
-		for(var i = 0; i < Tournament.INITIAL_AGENTS.length; i++) {
-			totalAgents += Tournament.INITIAL_AGENTS[i].count;
-		}
-		
-		var adjustedTotal = ensurePopulationMultipleOf3(totalAgents);
-		if (adjustedTotal !== totalAgents) {
-			// Adjust the random strategy count to reach target
-			var randomConfig = Tournament.INITIAL_AGENTS.find(function(config) {
-				return config.strategy === "random";
-			});
-			if (randomConfig) {
-				randomConfig.count += (adjustedTotal - totalAgents);
-			}
-		}
-		
-		var tournament = self.add({id:"tournament", type:"Tournament", x:-20, y:-20});
-		
+		self.add({id:"tournament", type:"Tournament", x:-20, y:-20});
+
 		// Screw it, just ALL of the Sandbox UI
 		self.add({id:"sandbox", type:"SandboxUI"});
 
-		// Expanded explanation text (no button) - 1.5x wider, moved down one line
+		// Label & Button for next...
 		self.add({
 			id:"label_next", type:"TextBox",
-			x:55, y:470, width:900, align:"left",
+			x:55, y:481, width:535, align:"right",
 			text_id: "sandbox_end"
+		});
+		self.add({
+			id:"button_next", type:"Button",
+			x:605, y:485, size:"long",
+			text_id:"sandbox_end_btn",
+			message: "slideshow/scratch"
 		});
 		
 	},
 	onend: function(self){
-		// Reset to normal mode
-		Tournament.resetGlobalVariables();
 		self.clear();
 	}
 
