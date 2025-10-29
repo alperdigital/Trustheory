dda58c3448af7074c30009238e795adcec185712
alperdigital <alperdigital@icloud.com>
2025-10-29 22:52:15 +0300

    feat(section8): add 'ya rastgele gruplar?' button to go to Section 9 (group_intra)

--- js/modules/section8/bootstrap.js
+++ js/modules/section8/bootstrap.js
@@ -45,6 +45,9 @@
                           "<span id=\"mod8-total-warning\" class=\"mod8-warning\"></span>"+
                           "<span id=\"mod8-status\"></span>"+
                        "</div>"+
+					   "<div class=\"mod8-next-row\">"+
+					      "<button id=\"mod8-next-s9\" class=\"mod8-next-btn\" aria-label=\"ya rastgele gruplar?\">ya rastgele gruplar?</button>"+
+					   "</div>"+
                     "</div>"+
                  "</div>"+
               "</div>"+
@@ -86,8 +89,17 @@
         });
         on(document.getElementById("mod8-stop"), "click", function(){ mod8_stopAutoplay(); try{ mod8_renderEvolution(); mod8_renderMembersTriangle(); }catch(e){} });
         on(document.getElementById("mod8-reset"), "click", function(){ mod8_stopAutoplay(); mod8_initState(); mod8_renderUI(); try{ mod8_renderEvolution(); mod8_renderMembersTriangle(); }catch(e){} });
-        var speedEl = document.getElementById("mod8-speed");
+		var speedEl = document.getElementById("mod8-speed");
         if(speedEl){ on(speedEl, "input", function(e){ speedMs = parseInt(e.target.value,10)||1000; if(autoplayTimer){ mod8_stopAutoplay(); mod8_startAutoplay(); } }); }
+
+		// Go to Section 9 (Group Intra) like Section 7's CTA
+		var nextBtn = document.getElementById('mod8-next-s9');
+		if(nextBtn){
+			on(nextBtn, 'click', function(){
+				try{ publish('slideshow/goto', ['group_intra']); }
+				catch(e){ try{ window.location.hash = '#group_intra'; }catch(_e){} }
+			});
+		}
 
         mounted = true;
         // Ensure member hats stay strategy-consistent during all renders (revertible by removing mod8-lock-hats)
