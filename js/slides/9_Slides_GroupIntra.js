efe830c4ba9d9c6b6bdadca57875e734198897a1
alperdigital <alperdigital@icloud.com>
2025-10-30 16:56:16 +0300

    perf: lazy-load Section 9 scripts on-demand from slide start; remove eager S9 script includes

--- js/slides/9_Slides_GroupIntra.js
+++ js/slides/9_Slides_GroupIntra.js
@@ -2,7 +2,22 @@ SLIDES.push({
 
 	id: "group_intra",
 	onstart: function(self){
-		try{ if(window.S9_mount) S9_mount(); }catch(e){ console.error("S9 mount error", e); }
+		// Lazy-load Section 9 scripts only when this slide starts
+		function ensureS9Loaded(cb){
+			if(window.S9_mount){ cb(); return; }
+			var files=[
+				"section9/js/S9_main.js",
+				"section9/js/S9_model.js",
+				"section9/js/S9_strategies.js",
+				"section9/js/S9_ui.js",
+				"section9/js/S9_payoffs_ui.js"
+			];
+			var i=0; function next(){ if(i>=files.length){ cb(); return; }
+				var s=document.createElement('script'); s.src=files[i++]; s.defer=true; s.onload=next; s.onerror=next; document.body.appendChild(s);
+			}
+			next();
+		}
+		ensureS9Loaded(function(){ try{ S9_mount(); }catch(e){ console.error("S9 mount error", e); } });
 	},
 	onend: function(self){
 		try{ if(window.S9_unmount) S9_unmount(); }catch(e){ console.error("S9 unmount error", e); }
